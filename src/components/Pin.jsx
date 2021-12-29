6d546856857e206cc7ebe2676e1c5354c09899b3
alex <castro.t.alex@gmail.com>
2021-12-29 13:05:29 -0500

    error

--- src/components/Pin.jsx
+++ src/components/Pin.jsx
@@ -6,7 +6,6 @@ import { AiTwotoneDelete } from 'react-icons/ai';
 import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 
 import { client, urlFor } from '../client';
-import { fetchUser } from '../utils/fetchUser';
 
 const Pin = ({ pin }) => {
   const [postHovered, setPostHovered] = useState(false);
@@ -16,7 +15,8 @@ const Pin = ({ pin }) => {
 
   const { postedBy, image, _id, destination } = pin;
 
-  const user = fetchUser();
+  const user = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
   const deletePin = (id) => {
     client
       .delete(id)
